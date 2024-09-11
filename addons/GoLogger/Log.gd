df503a852d988b371ac1163f801c33e1dab10bd3
Burloe <tomnils72@outlook.com>
2024-09-12 00:28:51 +0200

    Update Log.gd

--- addons/GoLogger/Log.gd
+++ addons/GoLogger/Log.gd
@@ -5,19 +5,19 @@ class_name Log
 ##
 ## For setup instructions, see the README.md or https://github.com/Burloe/GoLogger
 
-const DEVFILE = "res://GoLogger/game.log" ## Change this location to anywhere you want in your project files
+const DEVFILE = "res://addons/GoLogger/game.log" ## Change this location to anywhere you want in your project files
 const FILE = "user://log/game.log" ## This file can be accessed by selecting Project > Open User Data Folder in the top-left. 
 # Usually located in C:\Users\User\AppData\Roaming\Godot\app_userdata\Project\logs
 
 
-## Initializes the logging of game events. Called by SaveManager in the _ready() function or after [param clear_log()] is completed.
+## Starts a log session, recording game events. 
 static func start_session() -> void:
 	# Opening a file with WRITE will clear it of previous contents. 
 	var _f = FileAccess.open(DEVFILE if GoLogger.log_on_dev else FILE, FileAccess.WRITE) 
 	_f.store_line(str("[", Time.get_datetime_string_from_system(true, true), "] New session:")) 
 	GoLogger.session_status_changed.emit(true)
 
-
+## Stops the current session.
 static func stop_session() -> void:
 	if GoLogger.log_session:
 		if FileAccess.file_exists(DEVFILE if GoLogger.log_on_dev else FILE):
@@ -30,8 +30,11 @@ static func stop_session() -> void:
 			GoLogger.session_status_changed.emit(false)
 
 
-## Stores a log entry into the [code]gamelog.txt[/code] file. By default, the system will log the date and time on each log entry. Optionally, this can be prevented by calling function with 'false'. 
-static func entry(log_entry : String, include_date_time : bool = true) -> void:
+## Stores a log entry into the [code]gamelog.txt[/code] file. [param date_time_flag] is used to specify if the date and time format you want included with your entries.
+##[codeblock] 0 = date + time + log entry
+## 1 = time + log entry
+## 2 = log entry [/codeblock]
+static func entry(log_entry : String, date_time_flag : int = 0) -> void:
 	if GoLogger.log_session:
 		if FileAccess.file_exists(DEVFILE if GoLogger.log_on_dev else FILE):
 			# Store the old contents
@@ -40,8 +43,12 @@ static func entry(log_entry : String, include_date_time : bool = true) -> void:
 			_file.close()
 			# Add the new log_entry
 			var _f = FileAccess.open(DEVFILE if GoLogger.log_on_dev else FILE, FileAccess.WRITE)
-			var _date : String = str("\t[", Time.get_datetime_string_from_system( true, true), "]: ")
-			_f.store_line(str(_content, _date if include_date_time else ": ", log_entry))
+			var _dt : String
+			match date_time_flag:
+				0: _dt = str("\t[", Time.get_datetime_string_from_system(true, true), "] ")
+				1: _dt = str("\t[", Time.get_time_string_from_system(true), "] ")
+				2: _dt = ""
+			_f.store_line(str(_content, _dt, log_entry))
 			_f.close()
 		else: # File doesn't exist > Create new one 
 			var _file = FileAccess.open(DEVFILE if GoLogger.log_on_dev else FILE, FileAccess.WRITE)
