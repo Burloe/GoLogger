1365f2c341dd5b31013e728835b69cbbb578218a
Burloe <tomnils72@outlook.com>
2024-11-02 19:18:51 +0100

    Slow Dock progress. Getting somewhere with indexing

--- addons/GoLogger/Dock/GoLoggerDock.gd
+++ addons/GoLogger/Dock/GoLoggerDock.gd
@@ -1,7 +1,7 @@
 @tool
 extends TabContainer
 
-
+#region Settings
 @onready var tooltip : Panel = $Settings/HBoxContainer/ToolTip
 
 @onready var base_dir : LineEdit = $Settings/HBoxContainer/ColumnA/VBox/HBoxContainer/VBoxContainer2/BaseDirLineEdit
@@ -58,92 +58,155 @@ var canvas_layer_tt : String = "Sets the layer of the CanvasLayer containing the
 
 @onready var controller_start_btn : CheckButton = $Settings/HBoxContainer/ColumnD/VBoxContainer/ShowOnStartCheckButton
 var controller_start_tt : String = "Show the controller by default."
+#endregion
 
 @onready var controller_monitor_side__btn : CheckButton = $Settings/HBoxContainer/ColumnD/VBoxContainer/MonitorSideCheckButton
 var controller_monitor_side_tt : String = "Set the side of the controller the log file monitor panel."
 
 # Category tab
-@onready var category_add_btn : Button = $Categories/MarginContainer/VBoxContainer/GridContainer/AddButton
-@onready var category_container : GridContainer = $Categories/MarginContainer/VBoxContainer/GridContainer
+@onready var add_category_btn : Button = $Categories/MarginContainer/VBoxContainer/HBoxContainer/AddButton
+@onready var category_container : GridContainer = $Categories/MarginContainer/VBoxContainer/HBoxContainer/GridContainer
+@onready var open_dir_btn : Button = $Categories/MarginContainer/VBoxContainer/Panel/MarginContainer/HBoxContainer/OpenDirButton
 var category_scene = preload("res://addons/GoLogger/Dock/LogCategory.tscn")
-var cats : Array[Panel] = []
+var categ : Array[Array] = []
  
 var config = ConfigFile.new()
-const CONFIG_PATH = "user://GoLogs/settings.ini"
+const PATH = "user://GoLogger/settings.ini"
+
+signal update_index
+
+var _t : String = ""
 
 func _ready() -> void: 
-	category_add_btn.button_up.connect(add_category)
-	base_dir.text = Log.base_directory
-	for i in range(Log.categories.size()):
-		var _n = category_scene.instantiate()
-		_n.cat_name = Log.categories[i].category_name
-		_n.index = i
-		category_container.add_child(_n)
+	if Engine.is_editor_hint():
+		# Load/create settings.ini
+		if !FileAccess.file_exists(PATH):
+			var _a : Array[Array] = [
+				["game", 0],
+				["player", 1]
+			]
+			config.set_value("plugin", "categories", _a)
+			config.set_value("plugin", "base_directory", "user://GoLogger/")
 
+			config.set_value("settings", "log_header", 0)
+			config.set_value("settings", "autostart_session", true)
+			config.set_value("settings", "use_utc", false)
+			config.set_value("settings", "dash_separator", false)
+			config.set_value("settings", "limit_method", 0)
+			config.set_value("settings", "limit_actio", 0)
+			config.set_value("settings", "file_cap", 10)
+			config.set_value("settings", "entry_count_limit", 1000)
+			config.set_value("settings", "session_timer_wait_time", 600.0)
+			config.set_value("settings", "error_reporting", 0)
+			config.set_value("settings", "print_session_changes", 0)
+			config.set_value("settings", "disable_session_warning", false)
+			config.set_value("settings", "disable_entry_warning", false)
+			config.set_value("settings", "canvaslayer_layer", 5)
+			config.set_value("settings", "hide_controller", true)
+			config.set_value("settings", "controller_drag_offset", Vector2(0,0))
+			config.save(PATH)
+		else:
+			config.load(PATH)
+
+		add_category_btn.button_up.connect(add_category)
+		open_dir_btn.button_up.connect(open_directory)
+		base_dir.text = Log.base_directory
+
+		# Remove existing categories
+		for i in category_container.get_children():
+			if i is not Button:
+				i.queue_free()
+
+		# Add categories as saved in .ini file
+		load_categories()
 
 
 
 func _physics_process(delta: float) -> void:
+	$Categories/MarginContainer/VBoxContainer/Label.text = str("Current size(): ", categ.size(), "\n", categ, "\n", category_container.get_children(), _t)
 
-	tooltip.text 
+
+
+
+func load_categories() -> void:
+	categ = config.get_value("plugin", "categories")
+	for i in range(categ.size()):
+		var _n = category_scene.instantiate()
+		_n.dock = self
+		_n.category_name = categ[i][0]
+		_n.index = i
+		categ[i][1] = i
+		category_container.add_child(_n)
+		category_container.move_child(_n, _n.index)
+	update_indices()
+	config.save_value("plugin", "categories", categ)
+
 	
 ## Adds a new category element to the dock. Adds a corresponding [LogFileResource] to the [param categories].
-func add_category(name : String) -> void:
-	# Load/create settings.ini
-	if !FileAccess.file_exists(CONFIG_PATH):
-		var _a : Array[LogFileResource] = [preload("res://addons/GoLogger/Resources/DefaultLogFile.tres")]
-		config.set_value("plugin", "categories", _a)
-		config.set_value("plguin", "base_directory", "user://GoLogger/")
-		config.set_value("settings", "log_header", 0)
-		config.set_value("settings", "autostart_session", true)
-		config.set_value("settings", "use_utc", false)
-		config.set_value("settings", "dash_separator", false)
-		config.set_value("settings", "limit_method", 0)
-		config.set_value("settings", "limit_actio", 0)
-		config.set_value("settings", "file_cap", 10)
-		config.set_value("settings", "entry_count_limit", 1000)
-		config.set_value("settings", "session_timer_wait_time", 600.0)
-		config.set_value("settings", "error_reporting", 0)
-		config.set_value("settings", "print_session_changes", 0)
-		config.set_value("settings", "disable_session_warning", false)
-		config.set_value("settings", "disable_entry_warning", false)
-		config.set_value("settings", "canvaslayer_layer", 5)
-		config.set_value("settings", "hide_controller", true)
-		config.set_value("settings", "controller_drag_offset", Vector2(0,0))
-		config.save(CONFIG_PATH)
-	else:
-		config.load(CONFIG_PATH)
-	
+func add_category() -> void:
 	var _n = category_scene.instantiate()
-	_n.dock = self
+	_n.dock = self 
+	_n.index = categ.size()
 	category_container.add_child(_n)
-	cats.append(_n)
-	Log.categories.append(_n)
+	category_container.move_child(_n, _n.index)
+	categ.append([_n.category_name, categ.size()])
+	update_indices()
+	config.set_value("plugin", "categories", categ)
+	config.save(PATH)
 
 
-
-func change_category_name(current_name : String, new_name : String) -> void:
-	for i in Log.categories:
-		if i.category_name == current_name:
-			i.category_name = new_name
-
+func update_category_name(current_name : String, new_name : String) -> void:
+	# Check conflicts
+	for i in categ:
+		if i[0] == new_name:
+			return
+	# Update name
+	for i in categ:
+		if i[0] == current_name:
+			i[0] = new_name
+			save_setting("plugin", "categories")
+			break
+	update_indices()
+	
 
 ## Remove [LogFileResource] from array 
 func remove_category(name : String) -> void:
-	for i in range(Log.categories.size()):
-		if Log.categories[i].category_name == name:
-			Log.categories.remove_at(i)
+	print(str(name, categ))
+	for i in range(categ.size()):
+		print(str("Trying to remove ", i, " with array size = ", categ.size()))
+		if categ[i][0] == name:
+			categ.remove_at(i)
+			break  
+	update_indices()
+
+    
+func update_indices() -> void:
+	var _c = category_container.get_children()
+	for i in range(categ.size()):
+		categ[i][1] = i # updates array corresponding array
+		_c[i].index = i # updates actual dock elements
+		_c[i].update_index_label(i)
 
 
 
-func save_plugin_setting(section : String, key : String, value) -> void:
-	config.set_value(section, key, value)
-	config.save(CONFIG_PATH)
 
 
 
-func load_plugin_settings(section : String) -> Dictionary:
+func load_settings(section : String) -> Dictionary:
 	var settings = {}
 	for key in config.get_section_keys(section):
 		settings[key] = config.get_value(section, key)
 	return settings
+
+
+
+func save_setting(value, key : String, section : String = "settings") -> void:
+	config.set_value(section, key, value)
+	config.save(PATH) 
+
+
+
+func open_directory() -> void:
+	var abs_path = ProjectSettings.globalize_path(config.get_value("plugin", "base_directory"))
+	print(abs_path)
+	OS.shell_open(abs_path)
--- addons/GoLogger/Dock/LogCategory.gd
+++ addons/GoLogger/Dock/LogCategory.gd
@@ -1,33 +1,49 @@
 @tool
 extends Panel
 
-
+@onready var ilbl : Label = $MarginContainer/VBoxContainer/Label 
 @onready var line_edit : LineEdit = $MarginContainer/VBoxContainer/LineEdit
 @onready var del_btn : Button = $MarginContainer/VBoxContainer/DeleteButton
-var dock : Panel ## Dock root
+var dock : TabContainer ## Dock root
 
-@export var cat_name : String = "":
+
+@export var category_name : String = "":
 	set(value):
 		if value.length() <= 20:
-			dock.change_category_name(value)
-			cat_name = value
-@export var index : int = 0
+			if dock != null: dock.update_category_name(category_name, value)
+			# print(str("DOCK = ", dock))
+			category_name = value
+			if line_edit != null: line_edit.text = category_name
+			
+@export var index : int = 0:
+	set(value):
+		print(str("UPDATING FROM ", index, " TO ", value))
+		index = value
+		ilbl.text = str(value)
+		print(str("UPDATED LABEL NOW SAYS = ", ilbl.text))
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if Engine.is_editor_hint():
 		del_btn.button_up.connect(_on_del_button_up)
 		line_edit.text_changed.connect(_on_text_changed)
-		line_edit.text = cat_name
-		
+		line_edit.text_submitted.connect(_on_text_submitted)
+		line_edit.text = category_name
+		ilbl.text = str(index)
 
+
+func update_index_label(idx : int) -> void:
+	ilbl.text = str(idx)
 
 
 func _on_text_changed(new_text : String) -> void:
-	cat_name = new_text
+	dock._t = str("\n", index)
+
+func _on_text_submitted(new_text : String) -> void:
+	# dock._t = str("\n", index, " Submitted = ", new_text)
+	line_edit.release_focus()
 
 
-
-func _on_del_button_up() -> void:
-	dock.remove_category(cat_name)
+func _on_del_button_up() -> void: 
+	dock.remove_category(category_name)
 	queue_free() 
