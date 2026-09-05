0696b62212f6b4b3fa81828866d2de82c8308f24
Burloe <tomnils72@outlook.com>
2026-09-05 02:22:27 +0200

    Dragging implimented

--- addons/GoLogger/scripts/log_popup.gd
+++ addons/GoLogger/scripts/log_popup.gd
@@ -2,6 +2,7 @@
 extends PopupPanel
 
 @onready var top_bar: Panel = %TopBar
+@onready var resize_handle: TextureRect = %ResizeHandle
 @onready var copy_btn: Button = %CopyButton
 @onready var settings_btn: Button = %SettingsButton
 @onready var close_btn: Button = %CloseButton
@@ -10,6 +11,8 @@ extends PopupPanel
 var dragging: bool = false
 var first_popup := true
 var saved_position := Vector2i.ZERO
+
+var resizing := false
 
 var content: String = "":
 	set(value):
@@ -21,6 +24,7 @@ var content: String = "":
 func _ready() -> void:
 	assign_icons()
 	about_to_popup.connect(_on_about_to_popup)
+	resize_handle.gui_input.connect(_on_resize_handle_gui_input)
 	close_btn.button_up.connect(func() -> void: hide())
 	top_bar.gui_input.connect(_on_title_bar_gui_input)
 
@@ -57,3 +61,21 @@ func _on_title_bar_gui_input(event: InputEvent) -> void:
 	elif event is InputEventMouseMotion and dragging:
 		position += Vector2i(event.relative.x, event.relative.y)
 		saved_position = position 
+
+
+
+func _on_resize_handle_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			resizing = event.pressed
+
+	elif event is InputEventMouseMotion and resizing:
+		var new_size: Vector2i = Vector2i(
+			int(size.x) + event.relative.x,
+			int(size.y) + event.relative.y
+		)
+
+		new_size.x = max(int(new_size.x), 300)
+		new_size.y = max(int(new_size.y), 200)
+
+		size = new_size
